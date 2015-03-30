1d7b6fdad36083a8ae6921bd8aeb8e9925f433e8
Alex-Harris <Alex-Harris@appacademy.io>
2015-03-30 16:31:11 -0400

    refactor board

--- board.rb
+++ board.rb
@@ -7,15 +7,11 @@ class Board
   def initialize
     @board = Array.new(9) {Array.new(9)}
     create_board
-
   end
 
   def create_board
-
-    @board.each_with_index do |row, row_idx|
-      row.count.times do |col_idx|
-        @board[row_idx][col_idx] = Tile.new(self, [row_idx, col_idx])
-      end
+    act_on_board do |row_idx, col_idx|
+      @board[row_idx][col_idx] = Tile.new(self, [row_idx, col_idx])
     end
 
     assign_bombs
@@ -24,18 +20,27 @@ class Board
     nil
   end
 
-  def show_board
+  def act_on_board(&prc)
     @board.each_with_index do |row, row_idx|
       row.count.times do |col_idx|
-        tile = @board[row_idx][col_idx]
-        tile.revealed = true
+        prc.call(row_idx, col_idx)
       end
     end
+
+    nil
+  end
+
+  def show_board
+    act_on_board do |row_idx, col_idx|
+      tile = @board[row_idx][col_idx]
+      tile.revealed = true
+    end
+
+    nil
   end
 
   def assign_bombs
     pairs = []
-
     until pairs.length == BOMB_COUNT
       new_pair = [rand((0...9)),rand((0...9))]
       pairs << new_pair unless pairs.include?(new_pair)
@@ -45,17 +50,15 @@ class Board
       @board[pair[0]][pair[1]].bomb_count = nil
     end
 
+    nil
   end
 
   def update_bomb_count
-    @board.each_with_index do |row, row_idx|
-      row.count.times do |col_idx|
-        tile = @board[row_idx][col_idx]
-        tile.bomb_counter
-      end
+    act_on_board do |row_idx, col_idx|
+      tile = @board[row_idx][col_idx]
+      tile.bomb_counter
     end
 
+    nil
   end
-
-
 end
